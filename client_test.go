3c6fa6bd732eaee7d8726e55b05112254415d4cc
toyaha <51659862+toyaha@users.noreply.github.com>
2020-08-10 23:12:50 +0900

    refactor

--- client_test.go
+++ client_test.go
@@ -10,85 +10,6 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func TestClient_Query_SelectRow(t *testing.T) {
-	table := testItem{}
-	tests := []struct {
-		name     string
-		rec      *Client
-		setQuery func(*Query) *Query
-		dest     interface{}
-		wantErr  bool
-	}{
-		{
-			"select min",
-			nil,
-			func(query *Query) *Query {
-				query.SetSelect("min(", &table.Id, ") as min")
-				return query
-			},
-			&struct {
-				Min NullInt64 `column:"min" json:"min"`
-			}{},
-			false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name+" for mssql", func(t *testing.T) {
-			db, err := testNewClientMssql()
-			if err != nil {
-				t.Errorf("Query.SelectRow() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			defer func() {
-				_ = db.Close()
-			}()
-
-			query := db.NewQuery(&table)
-			query = tt.setQuery(query)
-			err = query.SelectRow(tt.dest)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Query.SelectRow() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-
-		t.Run(tt.name+" for mysql", func(t *testing.T) {
-			db, err := testNewClientMysql()
-			if err != nil {
-				t.Errorf("Query.SelectRow() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			defer func() {
-				_ = db.Close()
-			}()
-
-			query := db.NewQuery(&table)
-			query = tt.setQuery(query)
-			err = query.SelectRow(tt.dest)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Query.SelectRow() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-
-		t.Run(tt.name+" for postgresql", func(t *testing.T) {
-			db, err := testNewClientPostgresql()
-			if err != nil {
-				t.Errorf("Query.SelectRow() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			defer func() {
-				_ = db.Close()
-			}()
-
-			query := db.NewQuery(&table)
-			query = tt.setQuery(query)
-			err = query.SelectRow(tt.dest)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Query.SelectRow() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
 func TestClient_Query_SelectResultRows(t *testing.T) {
 	table := testItem{}
 	tests := []struct {
@@ -136,6 +57,51 @@ func TestClient_Query_SelectResultRows(t *testing.T) {
 	}
 }
 
+func TestClient_Query_SelectRow(t *testing.T) {
+	table := testItem{}
+	tests := []struct {
+		name     string
+		rec      *Client
+		setQuery func(*Query) *Query
+		dest     interface{}
+		wantErr  bool
+	}{
+		{
+			"select min",
+			nil,
+			func(query *Query) *Query {
+				query.SetSelect("min(", &table.Id, ") as min")
+				return query
+			},
+			&struct {
+				Min NullInt64 `column:"min" json:"min"`
+			}{},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		for _, databaseType := range []string{DatabaseTypeMssql, DatabaseTypeMysql, DatabaseTypePostgresql} {
+			t.Run(tt.name+" for "+databaseType, func(t *testing.T) {
+				db, err := testNewClientMssql()
+				if err != nil {
+					t.Errorf("Query.SelectRow() error = %v, wantErr %v", err, tt.wantErr)
+					return
+				}
+				defer func() {
+					_ = db.Close()
+				}()
+
+				query := db.NewQuery(&table)
+				query = tt.setQuery(query)
+				err = query.SelectRow(tt.dest)
+				if (err != nil) != tt.wantErr {
+					t.Errorf("Query.SelectRow() error = %v, wantErr %v", err, tt.wantErr)
+				}
+			})
+		}
+	}
+}
+
 func TestClient_Query_SelectResultRow(t *testing.T) {
 	table := testItem{}
 	tests := []struct {
@@ -158,7 +124,6 @@ func TestClient_Query_SelectResultRow(t *testing.T) {
 	}
 	for _, tt := range tests {
 		for _, databaseType := range []string{DatabaseTypeMssql, DatabaseTypeMysql, DatabaseTypePostgresql} {
-			fmt.Printf("%+v\n", databaseType)
 			t.Run(tt.name+" for "+databaseType, func(t *testing.T) {
 				db, err := testNewClient(databaseType)
 				if err != nil {
